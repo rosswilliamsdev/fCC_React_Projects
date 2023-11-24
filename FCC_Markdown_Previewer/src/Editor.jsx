e86bac8627612c1dc5d1828aa9a721ddbc626b66
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-11-24 09:53:39 -0600

    State and Effect hooks (incomplete)

--- FCC_Markdown_Previewer/src/Editor.jsx
+++ FCC_Markdown_Previewer/src/Editor.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect } from "react";
-import { marked } from 'marked'
+import { Marked, marked } from "marked";
 export default function Editor({ text, setText }) {
   const editor = document.getElementById("editor");
+  const prev = document.getElementById("preview");
 
+// will setting editor and preview text to state make it easier in long run?
   function saveText(event) {
-    let prev = document.getElementById('preview')
-    prev.innerHTML = marked.parse(event.target.value)
-    // setText(event.target.value);
+    setText(event.target.value);
+    prev.innerHTML = marked.parse(text);
     // console.log(text);
   }
 
+// User story #5: trying to set the default editor text to be html markdown
+
+  // useEffect(() => {
+  //   editor.innerHTML = marked.parse(text);
+  //   prev.innerHTML = marked.parse(text);
+  // }, [text]);
+
   return (
     <div>
-      {/* this onChange doesn't work */}
       <textarea id="editor" onChange={saveText}></textarea>
       <div>hi ross</div>
     </div>
