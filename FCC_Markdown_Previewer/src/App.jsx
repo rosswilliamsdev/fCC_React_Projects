bd82f6df334617c72e48069789360d2ef7d5b8a0
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-11-24 16:26:51 -0600

    switched to react-markdown from marked.js

--- FCC_Markdown_Previewer/src/App.jsx
+++ FCC_Markdown_Previewer/src/App.jsx
@@ -4,7 +4,27 @@ import Editor from "./Editor.jsx";
 import Preview from "./Preview";
 
 function App() {
-  const [text, setText] = useState("");
+
+const defaultMarkdown = `
+    # Welcome #
+
+    ## This is my first go with markdown ##
+
+    [Link](www.google.com)
+
+      <p>Here is a code block</p>
+
+    1. List item one
+    2. List item two
+
+    >This is a blockquote
+
+    ![Mountains rule](FCC_Markdown_Previewer/src/assets/matterhorn-BLOG-mountains.png)
+
+    Here is some **bold, bold** text
+    `;
+
+  const [text, setText] = useState(defaultMarkdown);
 
   return (
     <div>
--- FCC_Markdown_Previewer/src/Editor.jsx
+++ FCC_Markdown_Previewer/src/Editor.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Marked, marked } from "marked";
+import React from "react";
+
 export default function Editor({ text, setText }) {
   const editor = document.getElementById("editor");
   const prev = document.getElementById("preview");
@@ -7,41 +7,11 @@ export default function Editor({ text, setText }) {
   // will setting editor and preview text to state make it easier in long run?
   function saveText(event) {
     setText(event.target.value);
-    prev.innerHTML = marked.parse(event.target.value);
   }
-
-  // User story #5: trying to set the default editor text to be html markdown on page load
-
-  // When my markdown previewer first loads, the default text in the #editor field should contain valid markdown that represents at least one of each of the following elements: a heading element (H1 size), a sub heading element (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
-
-  const defaultMarkdown = `
-    # Welcome #
-
-    ## This is my first go with markdown ##
-
-    [Link](www.google.com)
-
-      <p>Here is a code block</p>
-
-    1. List item one
-    2. List item two
-
-    >This is a blockquote
-
-    ![Mountains rule](FCC_Markdown_Previewer/src/assets/matterhorn-BLOG-mountains.png)
-
-    Here is some **bold, bold** text
-    `;
-
-  console.log(marked.parse("#Welcome"));
 
   return (
     <div>
-      <textarea
-        id="editor"
-        onChange={saveText}
-        defaultValue={defaultMarkdown}
-      ></textarea>
+      <textarea id="editor" onChange={saveText} defaultValue={text}></textarea>
       <div>oh hi mark</div>
     </div>
   );
