4497685df6353dba52444dffa58fb7297621577c
thisismydisplay <markrockwellosman@gmail.com>
2023-12-02 12:38:46 -0600

    This is one approach to story 6

--- FCC_Drum_Machine/src/Buttons.jsx
+++ FCC_Drum_Machine/src/Buttons.jsx
@@ -1,10 +1,34 @@
 import { heaterKit } from "./heaterKit";
 
+const keyMap = {
+  81: 0,
+  87: 1,
+  69: 2,
+  65: 3,
+  83: 4,
+  68: 5,
+  90: 6,
+  88: 7,
+  67: 8,
+};
+
 export default function Buttons() {
+
+  document.addEventListener("keydown", (e)=>{
+    let code = String(e.keyCode)
+    if (Object.keys(keyMap).includes((code))){
+      let pads = document.querySelectorAll(".drum-pad")
+      let index = keyMap[code]
+      let pad = pads[index]
+      pad.children[0].play()
+    }
+  })
+
   function handleClick(e) {
     let audio = e.target.children[0];
     audio.play();
   }
+
   return (
     <div className="pad-grid">
       {heaterKit.map((element) => {
