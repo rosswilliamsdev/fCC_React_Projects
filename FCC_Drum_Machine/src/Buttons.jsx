72f6cbf8d08a3a3b89220163511794e0874cf127
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-12-02 11:45:30 -0600

    feat: story 5

--- FCC_Drum_Machine/src/Buttons.jsx
+++ FCC_Drum_Machine/src/Buttons.jsx
@@ -1,12 +1,25 @@
 import { heaterKit } from "./heaterKit";
 
 export default function Buttons() {
+  function handleClick(e) {
+    let audio = e.target.children[0];
+    audio.play();
+  }
   return (
     <div className="pad-grid">
       {heaterKit.map((element) => {
         return (
-          <div key={element.id} id={element.desc} className="drum-pad">
-            <audio className="clip" id={element.letter} src={element.sampleURL}/>
+          <div
+            key={element.id}
+            id={element.desc}
+            className="drum-pad"
+            onClick={(e) => handleClick(e)}
+          >
+            <audio
+              className="clip"
+              id={element.letter}
+              src={element.sampleURL}
+            />
             {element.letter}
           </div>
         );
