810c60534167b43767fba5c17cc5f69227fa2424
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-12-02 10:53:24 -0600

    for mork

--- FCC_Drum_Machine/src/App.jsx
+++ FCC_Drum_Machine/src/App.jsx
@@ -1,10 +1,11 @@
 import Button from "./Button";
+import "./App.css";
 
 export default function App() {
   return (
     <div id="drum-machine">
       <div id="display">
-          <Button />
+        <Button />
       </div>
     </div>
   );
