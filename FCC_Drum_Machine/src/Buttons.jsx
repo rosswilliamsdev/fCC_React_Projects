b68b1039c269dbd4866c6ba4f3b7b2ed3417317a
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-12-05 11:57:28 -0600

    feat: keydown triggers pad sounds

--- FCC_Drum_Machine/src/Buttons.jsx
+++ FCC_Drum_Machine/src/Buttons.jsx
@@ -1,33 +1,42 @@
 import { heaterKit } from "./heaterKit";
-
-const keyMap = {
-  81: 0,
-  87: 1,
-  69: 2,
-  65: 3,
-  83: 4,
-  68: 5,
-  90: 6,
-  88: 7,
-  67: 8,
-};
+import { useEffect } from "react";
 
 export default function Buttons() {
+  const handleKeyPress = (event) => {
+    // Define the key-to-sound mapping
+    const soundMap = {
+      q: heaterKit[0].sampleURL,
+      w: heaterKit[1].sampleURL,
+      e: heaterKit[2].sampleURL,
+      a: heaterKit[3].sampleURL,
+      s: heaterKit[4].sampleURL,
+      d: heaterKit[5].sampleURL,
+      z: heaterKit[6].sampleURL,
+      x: heaterKit[7].sampleURL,
+      c: heaterKit[8].sampleURL,
+    };
 
-  document.addEventListener("keydown", (e)=>{
-    let code = String(e.keyCode)
-    if (Object.keys(keyMap).includes((code))){
-      let pads = document.querySelectorAll(".drum-pad")
-      let index = keyMap[code]
-      let pad = pads[index]
-      pad.children[0].play()
+    // Check if the pressed key has a corresponding sound
+    const keyPressed = event.key.toLowerCase();
+    const soundFileName = soundMap[keyPressed];
+
+    // If a sound is mapped to the pressed key, play it
+    if (soundFileName) {
+      const audio = new Audio(soundFileName);
+      audio.play();
     }
-  })
-
+  };
   function handleClick(e) {
     let audio = e.target.children[0];
     audio.play();
   }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
 
   return (
     <div className="pad-grid">
